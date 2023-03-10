fa1da60e63098e3bd08f34773873de83906ab4d8
AliRayza <ali.raza1@students.mq.edu.au>
2023-03-10 13:02:34 +1100

    TCPClient.java

---
+++ TCPClient.java
@@ -0,0 +1,10 @@
+import java.net.Socket;
+import Java.net. InetAddress;
+import Java.to.Datalnputstream;
+import Java.io.DataoutputStrean;
+import java.util.concurrent.TimeUnit;
+
+public class TCPClient (
+ public static void main(String[] args){
+    
+ }
