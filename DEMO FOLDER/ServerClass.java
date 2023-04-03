997b05f8105f72e063e38f18d0ff79c5ba857474
Ali Rayza <Ali.raza1@students.mq.edu.au>
2023-04-03 23:32:16 +1000

    Final Working Solution - Last Minute Commit v2

--- DEMO FOLDER/ServerClass.java
+++ DEMO FOLDER/ServerClass.java
@@ -1,14 +1,12 @@
 public class ServerClass{
 
     String serverType; //[0]
-    int serverID; //[1]
-    int serverTypeNumber; //[2]
-    int CPUCoreNo; //[3]
+    int serverTypeNumber; //[1]
+    int CPUCoreNo; //[2]
 
    
-    public ServerClass(String serverType, int serverID, int serverTypeNumber, int CPUCoreNo ) {
+    public ServerClass(String serverType,int serverTypeNumber, int CPUCoreNo ) {
         this.serverType = serverType;
-        this.serverID = serverID;
         this.serverTypeNumber = serverTypeNumber;
         this.CPUCoreNo = CPUCoreNo;
 
--- DEMO FOLDER/TCPClient.java
+++ DEMO FOLDER/TCPClient.java
@@ -1,3 +1,4 @@
+
 /* Reference for code: 
  * 
  * Stage 1: Pseudo code for Largest Round Robin (LRR) https://content.ilearn.mq.edu.au/0b/1d/0b1dfdc5d985202002e9de6cbee2c743d3e1be3c?response-content-disposition=inline%3Bfilename%3D%22COMP3100_2023___Stage1_Pseudo_code.pdf%22&response-content-type=application%2Fpdf&Expires=1680293040&Signature=NeRuvp7efsftZEnroXEQGGgukvDZCdNiEty0RtfHblXH2JnPaq81E3Ou3BIaauKa0hH5sbzyrj4QnZDx716ZljGYF96dhs64zhjAZXGLdtESKBBqKaNzWlzyB5a7-eL8IacfgC4sYUwJJWd0S-DZDdbIIixz4zD282dBhqk79Mva7xJCHyyRzEL6CmbLIAWXpPXSOVXKLwwLttyPN-mm6U3JcU-ywuZyXfAFuq3ufMGieINvQGZN-Ca-KtI-lISHSjADCL7XTZW3SY-WKYXKdnCQFCKyzhgzGqKPWtKFJw~j7UjzXqVkA1IP7EzYOIAcAACMhDE~KPZ8O-mCfxcHEA__&Key-Pair-Id=APKAJAEFMXVVB5Z7N4TA
@@ -45,7 +46,7 @@ public static String handshakeProtocol() throws Exception{
 static void SCHDcommand(String command, int jobID, String serverType, int serverID) throws Exception{
 
         if(command.equals("JOBN")){ //checking that command is JOBN to perform SCHD
-            String schd_job = "SCHD " + jobID + " " + serverType + " " + serverID; //compiling all job details
+            String schd_job = "SCHD " + jobID + " " + serverType + " " + serverID; //compiling all job details //assigning serverID by computing the remainder of jobID and serverTypeNumber that corresponds to the pattern of serverID
              sendText(schd_job); //sending it to client 
              readText(); //receiving the message from the server
         }    
@@ -59,7 +60,7 @@ static void SCHDcommand(String command, int jobID, String serverType, int server
             in  = new BufferedReader(new InputStreamReader(s.getInputStream()));
 
             String serverType = ""; //serverType to keep track of the iteractive changes within code
-            int serverID = 0; //serverID to keep track of the iteractive changes within code
+           
             int coreNumbers = 0;//coreNumbers of the server to keep track of the iteractive changes within code
             int jobID = 0; //jobID retrieved from the jobList that needs to be assigned.
             int serverTypeNumber =1; //type number of the server
@@ -98,11 +99,11 @@ static void SCHDcommand(String command, int jobID, String serverType, int server
 
                     for(int i = 0; i < nRecs; i++) { //looping through all servers to find the largest servers 
                         String msg = readText();
-                        // if(msg.equals(".")){ break;} //checking that '.' is being sent at the start of the
+                       
                       
                         String [] LRRServer = msg.split(" ");//recording all largest server information
                         ArrayList<ServerClass> Server = new ArrayList<ServerClass>();
-                        Server.add(new ServerClass(LRRServer[0], serverID, serverTypeNumber, Integer.parseInt(LRRServer[4]))); //recording all server information into an arrayList
+                        Server.add(new ServerClass(LRRServer[0],serverTypeNumber, Integer.parseInt(LRRServer[4]))); //recording all server information into an arrayList
                         if  (Server.get(0).serverType.equals(serverType)){ //checking if server 
                             serverTypeNumber++; //incrementing the number of servers with same type by 1
 
@@ -125,8 +126,8 @@ static void SCHDcommand(String command, int jobID, String serverType, int server
                 
        
                 }
-                serverID=jobID%serverTypeNumber; //assigning serverID by using number of server types and jobID.
-                    SCHDcommand(jobList[0], jobID, serverType, serverID);
+
+                    SCHDcommand(jobList[0], jobID, serverType, jobID%serverTypeNumber);
                     
 
                 }
