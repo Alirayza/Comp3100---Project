d7d4ddc4563b74815d9c412fd6ed0499bc11d295
Alirayza <100833904+Alirayza@users.noreply.github.com>
2023-05-29 00:25:50 +1000

    Added new params

--- Assignment 2/ServerClass_w8.java
+++ Assignment 2/ServerClass_w8.java
@@ -2,13 +2,18 @@ public class ServerClass{
 
     String serverType; //[0]
     int serverTypeNumber; //[1]
-    int CPUCoreNo; //[2]
+    int availableCores;
+    int availableMemory;
+    int availableDisk;
 
    
-    public ServerClass(String serverType,int serverTypeNumber, int CPUCoreNo ) {
+    public ServerClass(String serverType,int serverTypeNumber, int availableCores, int availableMemory, int availableDisk ) {
         this.serverType = serverType;
         this.serverTypeNumber = serverTypeNumber;
-        this.CPUCoreNo = CPUCoreNo;
+        this.availableDisk = availableDisk;
+        this.availableCores = availableCores;
+        this.availableMemory = availableMemory;
+
 
     }
 }
